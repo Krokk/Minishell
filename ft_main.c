ed3e1824379cd5337cd498c0c1023b5891ec9bfe
Krokk <rfabre@e2r7p13.42.fr>
2017-08-17 17:51:39 +0200

    fork try

--- ft_main.c
+++ ft_main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_main.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: remi <remi@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: rfabre <rfabre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/13 17:26:28 by rfabre            #+#    #+#             */
-/*   Updated: 2017/08/17 12:26:04 by remi             ###   ########.fr       */
+/*   Updated: 2017/08/17 17:06:07 by rfabre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,36 @@ static void get_request(char **request)
         *request = ft_freejoin(*request, buf);
     }
 	if (!ret)
-	{
-		free(*request);
-		ft_putendl("DEBUG GET REQUEST");
-	}
-
+		ft_putendl("DEBUG READ");
     if (*(*request))
     {
         ft_putstr(*request);
         ft_putstr("\n");
-        free (*request);    
     }
-    else
-        free(*request);
-    
 }
-    
-            
+
+static void ft_execcommands(char **commands)
+{
+	pid_t pid;
+
+	pid = fork();
+	while (1)
+	{
+		if (pid == 0)
+		{
+			sleep(1);
+			ft_putendl("ok negro je lance la commande");
+			execve(commands[0], commands, NULL);
+			exit(0);
+		}
+		if (pid > 0)
+		{
+			break;
+			wait(0);
+		}
+	}
+}
+
 int main(int ac, char **argv, char **venv)
 {
     char *request;
@@ -48,14 +61,14 @@ int main(int ac, char **argv, char **venv)
 
     while (1)
     {
-    get_request(&request);  
-    // free(request);
-    if (ft_strstr(request, "exit"))
-        {
-            // free(request);
-            return (0);     
-               }
+    	get_request(&request);
+    	if (ft_strstr(request, "exit"))
+        	return (0);
+		commands = ft_strsplit(request,' ');
+		ft_strdel(&request);
+		free(request);
+		ft_strtrim(*commands);
+		ft_execcommands(commands);
     }
     return (0);
-
 }
--- minishell.h
+++ minishell.h
@@ -6,12 +6,14 @@
 /*   By: rfabre <rfabre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/15 15:33:22 by rfabre            #+#    #+#             */
-/*   Updated: 2017/08/15 15:35:38 by rfabre           ###   ########.fr       */
+/*   Updated: 2017/08/17 15:32:17 by rfabre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 # include "./libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
 
 #endif
